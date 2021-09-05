ba138f3b895dca42e89162d3d998a8d5245bb04f
nuzhat77r <61469465+nuzhat77r@users.noreply.github.com>
2021-09-05 21:04:59 +0600

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,20 @@
-from django.test import TestCase
 import unittest
+from django.test import SimpleTestCase
+from cart.views import *
 
+
+class TestForms(SimpleTestCase):
+
+    def cart_list(self):
+        form = ListForm(data={
+            'title': "product_title",
+            'qunatity': "product_quantity",
+        })
+        self.assertTrue(form.is_valid())
+
+    def cart_list(self):
+        form = ListForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 2)
 # Create your tests here.
